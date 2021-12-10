3afed76f099fa23b25345f11905e251fbf0d74ce
Peng-Yang0817 <83909356+Peng-Yang0817@users.noreply.github.com>
2021-12-10 08:58:23 +0800

    Update FilterConfig.cs

--- WebApplication2/App_Start/FilterConfig.cs
+++ WebApplication2/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using MvcApp.Filter;
 
 namespace WebApplication2
 {
@@ -8,6 +9,10 @@ namespace WebApplication2
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //授權過濾器
+            filters.Add(new MyAuthorizeAttribute());
+            //異常處理過濾器
+            filters.Add(new MyHandleErrorAttribute());
         }
     }
 }
