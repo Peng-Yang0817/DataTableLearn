99338b1061b5c01c0cf2c2b5ee1beaff6752e280
Peng-Yang0817 <83909356+Peng-Yang0817@users.noreply.github.com>
2021-12-10 08:57:07 +0800

    Add files via upload
    Filter4種實踐

---
+++ WebApplication2/Filter/MyAuthorizeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+namespace MvcApp.Filter
+{
+    /// <summary>
+    /// 授权过滤器
+    /// </summary>
+    public class MyAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            filterContext.HttpContext.Response.Write("OnAuthorization<br/>");
+            //注释掉父类方法，因为父类里的OnAuthorization方法会调用ASP.NET的授权验证机制
+            //base.OnAuthorization(filterContext);
+        }
+    }
+}
